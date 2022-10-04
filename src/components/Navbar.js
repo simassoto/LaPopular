a46d17bbf1f7fe12a5dd6c62064493ae73a7fee2
Carlos Soto <soto.simas@gmail.com>
2022-10-04 08:18:29 +0100

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
+import ReorderIcon from "@material-ui/icons/Reorder"
+import '../styles/Navbar.css';
 
 function Navbar() {
+
+    const [openLinks,setOpenLinks] = useState(false)
+
   return (
     <div className='navbar'>
-      <div className='rightside'>
+    <div className='leftSide'></div>
+      <div className='rightSide'>
+      <div className='hiddenLinks'>
         <Link to="/">Home</Link>
         <Link to="/menu">Menu</Link>
         <Link to="/art">Art</Link>
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
+      </div>
 
+        <button>
+          <ReorderIcon/>
+        </button>
       </div>
     </div>
   )
