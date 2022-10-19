07600cc4cfbc5e05459621d70f755d3e428ef16a
Carlos Soto <soto.simas@gmail.com>
2022-10-19 07:43:14 +0100

    background responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ function Footer() {
         <div className='leftSide'>
           <PlaceIcon/>
           <a target="_blank" href='https://www.google.com/maps/dir//La+Popular.+Taberna/@38.9654121,-9.4883222,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0xd1f2714104214af:0x205e92ff98675340!2m2!1d-9.4182822!2d38.965433' rel="noreferrer">
-          GET DIRECTIONS </a>
+          LOCATION </a>
         </div>
         <div className='center'>
           <AccessTimeIcon/>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import { useRef } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import Logo from "../assets/logo.png"
 import Bimba from "../assets/bimba.png"
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import InstagramIcon from "@material-ui/icons/Instagram";
 import FacebookIcon from "@material-ui/icons/Facebook";
 import "../styles/Navbar.css";
@@ -21,12 +22,18 @@ function Navbar() {
       </div>
       <nav ref={navRef}>
         <img className="toggle" alt="" src={Logo}></img>
-        <a href="/#">Home</a>
-        <a href="/#">Menu</a>
+        <a href="/#" id="home">Home </a>
+        <a href="/menu">Menu</a>
         <a href="/#">Art</a>
         <a href="/#">Press</a>
         <a href="/#">About Us</a>
         <a href="/#">Contact</a>
+        <div className="toggleh">
+          <AccessTimeIcon className="time"/>
+          <p>Mon - Fri:	<strong>17h - 23h</strong> <br />
+            Tue: <strong>Closed</strong> <br />
+            Sat & Sun :	<strong>11h - 23h</strong></p>
+        </div>
         <button className="nav-btn nav-close-btn" onClick={showNavbar}>
           <FaTimes />
         </button>
@@ -36,9 +43,8 @@ function Navbar() {
             <FaBars />
           </button>
           <div className="icons">
-            <InstagramIcon  style={{ height: 43, width: 26, color:"white" }}
-              alt=""/>  <FacebookIcon style={{ height: 43, width: 26, color:"white" }}
-              alt="" />
+            <InstagramIcon className="social"  style={{ height: 50, width: 33,  }}alt=""/>
+            <FacebookIcon className="social"  style={{ height: 50, width: 33, }} alt="" />
           </div>
         </div>
       </div>
