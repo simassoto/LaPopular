d7f87d5c20c8bba6567c4b520f6522bf9e7317ea
Carlos Soto <soto.simas@gmail.com>
2022-11-04 09:11:56 +0000

    test deploy

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router>
         <Navbar/>
         <Routes>
           <Route path="/"  element ={<Home/>}/>
